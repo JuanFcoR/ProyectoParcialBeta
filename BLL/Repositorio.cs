1ea5d3233b497d215639fe4cc06f117c04fcb98d
Juan Francisco Rosa Cuevas <juan_rosa1@ucne.edu.do>
2020-01-29 18:51:19 -0400

    Creando las interfaces repositorio

---
+++ BLL/Repositorio.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProyectoParcialBeta.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace ProyectoParcialBeta.BLL
+{
+    public class Repositorio<T> : IDisposable, IRepository<T> where T : class
+    {
+        internal Contexto _contexto;
+        public Repositorio()
+        {
+            _contexto = new Contexto();
+        }
+        public virtual T Buscar(int id)
+        {
+            T entity;
+            try
+            {
+                entity = _contexto.Set<T>().Find(id);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            ;
+            return entity;
+        }
+
+
+        public virtual List<T> GetList(Expression<Func<T, bool>> expression)
+        {
+            List<T> Lista = new List<T>();
+            try
+            {
+                Lista = _contexto.Set<T>().Where(p => true).ToList();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return Lista;
+        }
+
+
+        public virtual bool Guardar(T entity)
+        {
+            bool paso = false;
+            try
+            {
+                if (_contexto.Set<T>().Add(entity) != null)
+                    paso = _contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return paso;
+        }
+
+        public virtual bool Modificar(T entity)
+        {
+            bool paso = false;
+
+            try
+            {
+                _contexto.Entry(entity).State = EntityState.Modified;
+                paso = _contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return paso;
+        }
+
+        public virtual void Dispose()
+        {
+            _contexto.Dispose();
+        }
+    }
+}
