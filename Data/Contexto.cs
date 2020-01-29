f2949bee4b6dd7d2c3dc1e107e575015cea170eb
Juan Francisco Rosa Cuevas <juan_rosa1@ucne.edu.do>
2020-01-29 18:47:29 -0400

    Creando la cadena de conexion

---
+++ Data/Contexto.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectoParcialBeta.Data
+{
+    public class Contexto:DbContext
+    {
+        public DbSet<Prestamos> Prestamos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.\SqlExpress;Database=ProyectoParcialBetaDb;Trusted_Connection=true;");
+        }
+    }
+}
