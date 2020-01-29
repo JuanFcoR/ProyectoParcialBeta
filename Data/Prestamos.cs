8b878a867ba9a4bfdbbf921bacdfd7591bbd82fd
Juan Francisco Rosa Cuevas <juan_rosa1@ucne.edu.do>
2020-01-29 18:36:02 -0400

    Add project files.

---
+++ Data/Prestamos.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectoParcialBeta.Data
+{
+    public class Prestamos
+    {
+        public int PrestamoId { get; set; }
+        public DateTime Fecha { get; set; }
+        public decimal Monto { get; set; }
+        public int Semanas { get; set; }
+        public string Concepto { get; set; }
+
+        public Prestamos()
+        {
+            PrestamoId = 0;
+            Fecha = DateTime.Now;
+            Monto = 0;
+            Semanas = 0;
+            Concepto = String.Empty;
+        }
+    }
+}
